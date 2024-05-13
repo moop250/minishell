91eff506a6f4e79f3cf8c6a5da56e6f60fca8f1b
hlibine <hugo.libine@proton.me>
2024-05-13 17:26:59 +0200

    general modifications

--- srcs/lexing/lexing.c
+++ srcs/lexing/lexing.c
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:03:05 by hlibine           #+#    #+#             */
-/*   Updated: 2024/05/08 12:49:21 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/05/13 15:31:51 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ static int	quote_seperator(const char *input, const int start)
 		++pos;
 	if (input[pos] == input[start])
 			return (pos + 1);
-	ms_error(ft_strjoin("no ending quote at ", ft_itoa(pos)));
-	return (0);
+	ms_printerror(ft_strjoin("no ending quote at ", ft_itoa(pos)));
+	return (-1);
 }
 
 static int	seperator(const char *str, int pos)
@@ -37,8 +37,11 @@ static int	seperator(const char *str, int pos)
 		while (str[++pos] == tmp)
 			++count;
 		if (((tmp == '<' || tmp == '>') && count > 2) || (tmp == '|' && count > 1))
-			ms_error(ft_strjoin("ivalid redirection or pipe at ",
+		{
+			ms_printerror(ft_strjoin("ivalid redirection or pipe at ",
 				ft_itoa(pos)));
+			return (-1);
+		}
 	}
 	else
 	{
@@ -48,7 +51,7 @@ static int	seperator(const char *str, int pos)
 	return (pos);
 }
 
-void tokenizer(const char *input, t_core *core)
+int tokenizer(const char *input, t_core *core)
 {
 	int		pos[2];
 	int		i;
@@ -63,8 +66,14 @@ void tokenizer(const char *input, t_core *core)
 			pos[1] = quote_seperator(input, pos[0]);
 		else
 			 pos[1] = seperator(input, pos[0]);
+		if (pos[1] == -1)
+		{
+			ms_tokensclear(core->token);
+			return -1;
+		}
 		ms_addtoken_back(core,
 			ms_newtoken(ft_substr(input, pos[0], pos[1] - pos[0])));
 		i = pos[1];
 	}
+	return (1);
 }
