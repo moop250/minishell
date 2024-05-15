d4e3d2c71ecc2798da726b6cbc647af567c66619
moop250 <hugo.libine@proton.me>
2024-05-15 16:51:41 +0200

    Update errors.c

--- srcs/error_handling/errors.c
+++ srcs/error_handling/errors.c
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 16:06:33 by hlibine           #+#    #+#             */
-/*   Updated: 2024/05/15 14:35:34 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/05/15 16:51:31 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	ms_printerror(char *in)
 {
-	ft_printf_fd(2, "\033[1;31mminishell error: %s\033[0m\n", in);
+	ft_printf_fd(2, "\033[1;31mminishell error: %s\033[0m", in);
 }
 
 void	ms_error(char *in)
 {
-	ms_printerror(in);
+	ft_printf_fd(2, "\033[1;31mminishell error: %s\033[0m", in);
 	razegarbage();
 	exit(EXIT_FAILURE);
 }
