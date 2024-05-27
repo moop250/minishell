7f47ff63d41ba3f212092ac327a15681256d3fe1
hlibine <hugo.libine@proton.me>
2024-05-27 15:41:04 +0200

    Update main_loop.c
    updated prompt

--- srcs/minishell_loop/main_loop.c
+++ srcs/minishell_loop/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 14:00:53 by hlibine           #+#    #+#             */
-/*   Updated: 2024/05/23 14:53:08 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/05/27 15:37:07 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 char *make_prompt(t_core *core)
 {
-	char *prompt = core->prompt;
+	char	*prompt = core->prompt;
+	char	*tmp;
 
 	if (prompt != NULL)
 		gfree(prompt);
 	if (core->env->hasenv == false)
 		prompt = ft_strjoin(&core->argv[0][2], "$ ");
 	else
-		prompt = ft_strdup("test: ");
+	{
+		tmp = ft_strjoin("minshell:\033[1;34m", findenv("PWD")->value);
+		prompt = ft_strjoin(tmp, "\033[0m$ ");
+		gfree(tmp);
+	}
 	return (prompt);
 }
 
@@ -62,7 +67,6 @@ t_core	*minishell_loop(int ac, char **av, char **env)
 		printf("\n");
 		ms_tokensclear(&core->token);
 	}
-	ms_env(core);
 	gfree(core->line);
 	return (core);
 }
