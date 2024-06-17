4cf72bc7db4f963638ba355750f7cc2a295b80b9
moop250 <hugo.libine@proton.me>
2024-06-17 15:47:21 +0200

    changed how the memory for the params is allocated and how params are set
    still leaks though ;~;

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,31 +6,29 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:45:37 by hlibine           #+#    #+#             */
-/*   Updated: 2024/06/13 18:34:50 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2024/06/17 15:28:40 by hlibine          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 #include <stdio.h>
 
-static void	inputdelimiter(t_pipeline *pipe, t_token *token)
+static void	setdelimiter(t_pipeline *pipe, t_token *token, int status)
 {
 	t_pipe_fd	*tmp;
-
-	tmp = ms_addpipe_fd_back(pipe->pipeline_in);
-	if (token->content[1])
-		tmp->heredoc = true;
-	tmp->file_name = ft_strdup(token->next->content);
-	token = token->next;
-}
-
-static void	outputdelimiter(t_pipeline *pipe, t_token *token)
-{
-	t_pipe_fd	*tmp;
-
-	tmp = ms_addpipe_fd_back(pipe->pipeline_out);
-	if (token->content[1])
-		tmp->append = true;
+	
+	if (status == 1)
+	{
+		tmp = ms_addpipe_fd_back(pipe->pipeline_in);
+		if (token->content[1])
+			tmp->heredoc = true;
+	}
+	else
+	{
+		tmp = ms_addpipe_fd_back(pipe->pipeline_out);
+		if (token->content[1])
+			tmp->append = true;
+	}
 	tmp->file_name = ft_strdup(token->next->content);
 	token = token->next;
 }
@@ -48,13 +46,14 @@ static void	quotewrk(t_pipeline *pipe, t_token *token)
 			pipe->cmd = ft_substr(str, 1, ft_strlen(str) - 2);
 	else
 	{
-		i = pipe->param_count;
-		pipe->params = ft_realloc(pipe->params, i, (i + 1) * sizeof(char *));
+		i = 0;
+		if (pipe->params[0])
+			while (pipe->params[i])
+				++i;
 		if (str[0] == '\'')
 			pipe->params[++i] = ft_substr(str, 1, ft_strlen(str) - 2);
 		else
 			pipe->params[++i] = parse_quotes(str);
-		++pipe->param_count;
 	}
 }
 
@@ -73,11 +72,29 @@ static void	cmdwrk(t_pipeline **pipe, t_token *token)
 	}
 	else
 	{
-		i = (*pipe)->param_count;
-		(*pipe)->params = ft_realloc((*pipe)->params, i, (i + 1) * sizeof(char *));
+		i = 0;
+		if ((*pipe)->params[0])
+			while ((*pipe)->params[i])
+				++i;
 		(*pipe)->params[i] = ft_strdup(token->content);
-		++(*pipe)->param_count;
+		(*pipe)->params[i + 1] = NULL;
 	}
+}
+
+static int setparamcount(t_token *tmp)
+{
+	int		count;
+
+	count = -1;
+	while (tmp && tmp->content[0] != '|')
+	{
+		if (tmp->content[0] == '<' || tmp->content[0] == '>')
+			tmp = tmp->next;
+		else
+			++count;
+		tmp = tmp->next;
+	}
+	return (count);
 }
 
 void	parser(t_core *core, t_token *token)
@@ -87,6 +104,9 @@ void	parser(t_core *core, t_token *token)
 	while (token)
 	{
 		pipe = ms_addpipeline_back(core);
+		pipe->param_count = setparamcount(token);
+		if (pipe->param_count)
+			pipe->params = galloc((pipe->param_count + 1) * sizeof(char *));
 		while (token)
 		{
 			if (token->content[0] == '|')
@@ -95,19 +115,12 @@ void	parser(t_core *core, t_token *token)
 				break ;
 			}
 			else if (token->content[0] == '<')
-				inputdelimiter(pipe, token);
+				setdelimiter(pipe, token, 1);
 			else if (token->content[0] == '>')
-				outputdelimiter(pipe, token);
+				setdelimiter(pipe, token, 2);
 			else
 				cmdwrk(&pipe, token);
 			token = token->next;
-		}
-		pipe->params[0] = strdup("ur mom");
-		if (pipe->param_count > 0)
-		{
-			pipe->params = ft_realloc(pipe->params, pipe->param_count,
-				(pipe->param_count + 1) * sizeof(char *));
-			pipe->params[pipe->param_count] = NULL;
 		}
 	}
 	ms_tokensclear(&core->token);
