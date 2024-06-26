84cdf44f18d1be3a3217bc861600fc8b759dd6a6
pberset <pberset@student.42lausanne.ch>
2024-06-26 21:31:49 +0200

    pipe works on 2 commands

--- srcs/execution/pipe.c
+++ srcs/execution/pipe.c
@@ -6,7 +6,7 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 15:03:44 by pberset           #+#    #+#             */
-/*   Updated: 2024/06/24 18:45:15 by pberset          ###   ########.fr       */
+/*   Updated: 2024/06/26 21:30:25 by pberset          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,49 @@
 
 void	exec_pipes(t_core *core, char **env)
 {
-	int		pipe_fd[2];
-	int		pid;
+	int		fd[2];
+	int		child1_pid;
+	int		child2_pid;
+	int		i;
 	char	*exec_path;
 
-	if (pipe(pipe_fd) == -1)
+	i = 0;
+	while (i < core->pipe_count)
 	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		close(pipe_fd[0]);
-		dup2(pipe_fd[1], STDOUT_FILENO);
-		close(pipe_fd[1]);
 		exec_path = find_exec_path(core->pipeline->params[0], core->env->paths);
-		if (execve(exec_path, core->pipeline->params, env) == -1)
-			ms_error("execve error\n");
-		core->pipeline = core->pipeline->next;
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		close(pipe_fd[1]);
-		dup2(pipe_fd[0], STDIN_FILENO);
-		close(pipe_fd[0]);
-		waitpid(pid, NULL, 0);
+		if (pipe(fd) == -1)
+			ms_error("pipe error\n");
+		child1_pid = fork();
+		if (child1_pid == -1)
+			ms_error("child1 error\n");
+		if (child1_pid == 0)
+		{
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[0]);
+			close(fd[1]);
+			if (execve(exec_path, core->pipeline->params, env) == -1)
+				ms_error("execve1 error\n");
+		}
+		if (core->pipeline->next)
+		{
+			core->pipeline = core->pipeline->next;
+			exec_path = find_exec_path(core->pipeline->params[0], core->env->paths);
+			child2_pid = fork();
+			if (child2_pid == -1)
+				ms_error("child2 error\n");
+			if (child2_pid == 0)
+			{
+				dup2(fd[0], STDIN_FILENO);
+				close(fd[1]);
+				close(fd[0]);
+				if (execve(exec_path, core->pipeline->params, env) == -1)
+					ms_error("execve2 error\n");
+			}
+		}
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(child1_pid, NULL, 0);
+		waitpid(child2_pid, NULL, 0);
+		i++;
 	}
 }
