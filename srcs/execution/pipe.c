75037f67cb43179b7a6221ba3a261ab06fbafc68
sukori <sukori98@gmail.com>
2024-06-28 15:19:29 +0200

    waitpid works correctly

--- srcs/execution/pipe.c
+++ srcs/execution/pipe.c
@@ -6,7 +6,7 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 15:03:44 by pberset           #+#    #+#             */
-/*   Updated: 2024/06/27 17:26:31 by pberset          ###   LAUSANNE.ch       */
+/*   Updated: 2024/06/28 15:14:37 by pberset          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@ void	exec_pipes(t_core *core, char **env)
 {
 	int		fd[2];
 	int		prev_fd;
-	int		child_pid;
+	int		*child_pid;
 	int		i;
 	char	*exec_path;
 
 	i = 0;
 	prev_fd = -1;
+	child_pid = (int *)malloc((core->pipe_count + 1) * sizeof(int));
+	if (!child_pid)
+		ms_error("malloc error\n");
 	while (i <= core->pipe_count)
 	{
 		exec_path = find_exec_path(core->pipeline->params[0], core->env->paths);
 		if (pipe(fd) == -1)
 			ms_error("pipe error\n");
-		child_pid = fork();
-		if (child_pid == -1)
+		child_pid[i] = fork();
+		if (child_pid[i] == -1)
 			ms_error("child error\n");
-		if (child_pid == 0)
+		if (child_pid[i] == 0)
 		{
 			if (prev_fd != -1)
 			{
@@ -57,6 +60,8 @@ void	exec_pipes(t_core *core, char **env)
 		core->pipeline = core->pipeline->next;
 		i++;
 	}
-	while (++i <= core->pipe_count)
-		wait(NULL);
+	i = 0;
+	while (i++ <= core->pipe_count)
+			waitpid(child_pid[i], &(core->exit_status), 0);
+	free(child_pid);
 }
