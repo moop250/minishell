af4636c39b468e422ba6007a0e74d09177a08a49
sukori <sukori98@gmail.com>
2024-07-01 18:54:52 +0200

    pipe_loop

--- srcs/execution/execute.c
+++ srcs/execution/execute.c
@@ -6,7 +6,7 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 15:03:44 by pberset           #+#    #+#             */
-/*   Updated: 2024/07/01 18:48:03 by pberset          ###   ########.fr       */
+/*   Updated: 2024/07/01 18:53:42 by pberset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,39 +36,46 @@ static void	exec_child(int i, t_core *core, int *fd, char **env)
 		ms_error("execve error\n");
 }
 
+static void	pipe_loop(t_core *core, int *child_pid, char **env)
+{
+	int	i;
+	int	fd[2];
+
+	i = 0;
+	while (i <= core->pipe_count)
+	{
+	core->pipeline->execp = init_execp(core);
+	if (pipe(fd) == -1)
+		ms_error("pipe error\n");
+	child_pid[i] = fork();
+	if (child_pid[i] == -1)
+		ms_error("child error\n");
+	if (child_pid[i] == 0)
+		exec_child(i, core, fd, env);
+	else
+	{
+		if (core->prev_fd != -1)
+			close(core->prev_fd);
+		close(fd[1]);
+		core->prev_fd = fd[0];
+	}
+	core->pipeline = core->pipeline->next;
+	i++;
+	}
+}
+
 void	execute(t_core *core, char **env)
 {
-	int		fd[2];
 	int		*child_pid;
 	int		i;
 	t_pipeline	*tmp_pipe;
 
-	i = 0;
 	tmp_pipe = core->pipeline;
 	core->prev_fd = -1;
 	child_pid = (int *)malloc((core->pipe_count + 1) * sizeof(int));
 	if (!child_pid)
 		ms_error("malloc error\n");
-	while (i <= core->pipe_count)
-	{
-		core->pipeline->execp = init_execp(core);
-		if (pipe(fd) == -1)
-			ms_error("pipe error\n");
-		child_pid[i] = fork();
-		if (child_pid[i] == -1)
-			ms_error("child error\n");
-		if (child_pid[i] == 0)
-			exec_child(i, core, fd, env);
-		else
-		{
-			if (core->prev_fd != -1)
-				close(core->prev_fd);
-			close(fd[1]);
-			core->prev_fd = fd[0];
-		}
-		core->pipeline = core->pipeline->next;
-		i++;
-	}
+	pipe_loop(core, child_pid, env);
 	i = 0;
 	while (i++ <= core->pipe_count)
 			waitpid(child_pid[i], &(core->exit_status), 0);
