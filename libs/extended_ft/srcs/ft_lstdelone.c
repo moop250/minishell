5785ccb737ca82d68a470b294d643cfa5ae875c6
hlibine <hugo.libine@proton.me>
2024-07-03 20:32:14 +0200

    long overdue extended_ft changes

--- libs/extended_ft/srcs/ft_lstdelone.c
+++ libs/extended_ft/srcs/ft_lstdelone.c
@@ -6,17 +6,17 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:29:46 by hlibine           #+#    #+#             */
-/*   Updated: 2024/02/09 15:43:20 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/07/03 20:12:47 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "extended_ft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst)
 {
-	if (!lst || !del)
+	if (!lst)
 		return ;
-	del(lst->content);
+	gfree(lst->content);
 	gfree(lst);
 	return ;
 }
