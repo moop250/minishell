b975afe5e28e072b3e24efc85d03a5f9bf425ad1
moop250 <hugo.libine@proton.me>
2024-07-16 11:48:49 +0200

    unset somehow got lost during a commit ?

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,10 +6,29 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 16:57:39 by pberset           #+#    #+#             */
-/*   Updated: 2024/06/29 16:58:23 by pberset          ###   ########.fr       */
+/*   Updated: 2024/07/16 11:47:18 by hlibine          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
- * For each name, remove the corresponding variable or  function.
- * */
+#include "builtins.h"
+
+void	ms_unset(char **in, t_core *core)
+{
+	t_envparam	*tmp;
+	int			i;
+
+	i = 0;
+	while (in[++i])
+	{
+		tmp = findenv(in[i]);
+		if (tmp)
+		{
+			tmp->prev->next = tmp->next;
+			tmp->next->prev = tmp->prev;
+			gfree(tmp->name);
+			gfree(tmp->value);
+			gfree(tmp);
+		}
+	}
+	core->exit_status = 0;
+}
