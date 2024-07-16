b4b65a8f2bd1332e17a5985b34a0353b36d911f3
moop250 <hugo.libine@proton.me>
2024-07-16 15:57:35 +0200

    restructuring

--- srcs/initialization/init.c
+++ srcs/initialization/init.c
@@ -6,7 +6,7 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:20:59 by hlibine           #+#    #+#             */
-/*   Updated: 2024/07/16 14:29:26 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2024/07/16 15:55:15 by hlibine          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,28 @@ static void	ms_update(t_core *core)
 	core->env->cwd = ms_getcwd();
 }
 
+static void	set_env(t_core *core, char **env)
+{
+	core->env = galloc(sizeof(t_env));
+	core->env->envp = env;
+	core->env->rawenvs = NULL;
+	core->env->cwd = ms_getcwd();
+}
+
 t_core	*init(int ac, char **av, char **env)
 {
 	static t_core	*core;
 
 	if (core)
+	{
+		ms_update(core);
 		return (core);
+	}
 	if (ac > 1)
 		ms_error("minishell called with argument");
 	core = galloc(sizeof(t_core));
-	core->env = galloc(sizeof(t_env));
-	core->env->envp = env;
-	core->env->rawenvs = NULL;
-	core->env->cwd = ms_getcwd();
 	core->token = NULL;
+	set_env(core, env);
 	core->argc = ac;
 	core->argv = av;
 	if (!env[0])
