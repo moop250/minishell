b66082914b6b7e7e03aa5ea0a94f9dd9331626c7
Hugo <hugo.libine@proton.me>
2024-07-17 17:28:46 +0200

    work on export builtin

--- srcs/builtins/builtins.h
+++ srcs/builtins/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:18:04 by hlibine           #+#    #+#             */
-/*   Updated: 2024/07/16 16:11:36 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2024/07/17 16:21:29 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ void	ms_env(char **in, t_core *core);
 void	ms_export(char **in, t_core *core);
 void	ms_echo(char **in, t_core *core);
 void	ms_pwd(char **in, t_core *core);
+void	ms_exit(t_core *core, char **in);
 
 #endif
--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -3,18 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 16:54:21 by pberset           #+#    #+#             */
-/*   Updated: 2024/07/16 13:29:26 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2024/07/17 17:20:00 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-void	print_export(void)
+static char	**env_sorter(t_envparam *env)
 {
+	t_envparam	*tmp;
+	char		**out;
 
+	return (out);
+}
+
+static void	print_export(t_core *core)
+{
+	char	**sorted_in;
+	size_t	i;
+
+	i = -1;
+	sorted_in = env_sorter(core->env->rawenvs);
+	while (sorted_in[++i])
+		ft_printf_fd(core->ms_stdout, "declare -x %s\n", sorted_in[i]);
+	i = -1;
+	ft_2dfree(sorted_in);
 }
 
 void	ms_export(char **in, t_core *core)
@@ -24,7 +40,7 @@ void	ms_export(char **in, t_core *core)
 
 	i = 0;
 	if (in[1] == NULL)
-		print_export();
+		print_export(core);
 	while (in[++i])
 	{
 		param = findenv(in[i]);
