d61323dd03b6d090dc8fb988d17efd977fb75c2a
sukori <sukori98@gmail.com>
2024-07-29 16:03:39 +0200

    pid 2d free segfault

--- srcs/execution/execute.c
+++ srcs/execution/execute.c
@@ -6,24 +6,11 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 14:17:16 by pberset           #+#    #+#             */
-/*   Updated: 2024/07/29 13:51:01 by pberset          ###   ########.fr       */
+/*   Updated: 2024/07/29 16:01:07 by pberset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-static void	ignore_all_signals(void)
-{
-	int	sig;
-
-	sig = 1;
-	while (sig < _NSIG)
-	{
-		if (sig != SIGKILL && sig != SIGSTOP)
-			signal(sig, SIG_IGN);
-		sig++;
-	}
-}
 
 static int	close_pipes(int i, int pipe_count, int pipes[2][2])
 {
@@ -44,7 +31,6 @@ static int	close_pipes(int i, int pipe_count, int pipes[2][2])
 
 static int	child_exec(t_core *core, int pipes[2][2], int i)
 {
-	ignore_all_signals();
 	if (i < core->pipe_count || i > 0)
 		init_pipes(core->pipeline, pipes, i, core->pipe_count);
 	handle_redirections(core->pipeline);
@@ -56,25 +42,25 @@ int	execute(t_core *core)
 	int		pipes[2][2];
 	int		i;
 	int		status;
-	pid_t	pid;
+	pid_t	*pid;
 
+	pid = (pid_t *)galloc((core->pipe_count + 1) * sizeof(pid_t));
 	if (!core->pipe_count && is_builtin(core->pipeline->params[0]))
 		return (execute_builtins(core));
-	i = 0;
-	while (i < core->pipe_count + 1)
+	i = -1;
+	while (++i < core->pipe_count + 1)
 	{
 		if (i < core->pipe_count)
 			pipe(pipes[i % 2]);
-		pid = fork();
-		if (pid == 0)
+		pid[i] = fork();
+		if (pid[i] == 0)
 			child_exec(core, pipes, i);
-		else
-			waitpid(pid, &status, 0);
 		close_pipes(i, core->pipe_count, pipes);
 		core->pipeline = core->pipeline->next;
-		i++;
 	}
-	while (wait(&status) > 0)
-		;
+	i = -1;
+	while (++i < core->pipe_count + 1)
+		waitpid(pid[i], &status, 0);
+	ft_2dfree((void *)pid);
 	return (WEXITSTATUS(status));
 }
