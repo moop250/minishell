ae8abe834feae863058296f535123a760f076ea6
moop250 <hugo.libine@proton.me>
2024-08-01 21:11:24 +0200

    fixed unset fully :pray:

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 16:57:39 by pberset           #+#    #+#             */
-/*   Updated: 2024/07/24 17:24:50 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2024/08/01 21:08:42 by hlibine          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,24 @@
 int	ms_unset(char **in)
 {
 	t_envparam	*tmp;
+	t_envparam	**head;
 	int			i;
 
 	i = 0;
+	head = &get_core()->env->rawenvs;
 	while (in[++i])
 	{
 		tmp = findenv(in[i]);
 		if (tmp)
 		{
-			if (tmp->prev)
-				tmp->prev->next = tmp->next;
-			if (tmp->next)
-				tmp->next->prev = tmp->prev;
 			gfree(tmp->name);
 			gfree(tmp->value);
+			if (tmp->prev)
+				tmp->prev->next = tmp->next;
+			else
+				(*head) = (*head)->next;
+			if (tmp->next)
+				tmp->next->prev = tmp->prev;
 			gfree(tmp);
 		}
 	}
